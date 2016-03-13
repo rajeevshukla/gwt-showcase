0593580fdb5a813924135c90bcd796ef6680eb4e
Rajeev Shukla <mail2rajeevshukla@gmail.com>
2016-03-13 18:17:02 +0530

    added service action classes

---
+++ StrutsGWTIntegration/src/com/demo/mvp/action/GetDataServiceAction.java
@@ -0,0 +1,14 @@
+package com.demo.mvp.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GetDataServiceAction extends ActionSupport {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	
+	
+	
+}
---
+++ StrutsGWTIntegration/src/com/demo/mvp/shared/GetDataViewService.java
@@ -0,0 +1,6 @@
+package com.demo.mvp.shared;
+
+public interface GetDataViewService extends RemoteService {
+
+	public String getData(String name);
+}
